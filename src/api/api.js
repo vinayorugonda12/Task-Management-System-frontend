8a2d7fa8bf06b78e3b3a248001bf137b5262320a
vinay orugonda <vinayorugonda15@gmail.com>
2026-02-03 15:08:37 +0530

    api updated

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://task-management-system-589e.onrender.com";
+const BASE_URL = "https://task-management-system-589e.onrender.com/api/tasks";
 
 // Get all tasks
 export const getTasks = () => {
